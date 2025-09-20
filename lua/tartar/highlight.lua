47413dae07e9e1a0b8713a53fa886c2da599ee41
tar80 <tar80master@gmail.com>
2025-09-21 00:04:25 +0900

    refactor(highlight): fade_color function

--- lua/tartar/highlight.lua
+++ lua/tartar/highlight.lua
@@ -95,28 +95,29 @@ end
 ---@param attenuation number
 ---@return boolean ok, string RGB
 function M.fade_color(rgb, attenuation)
-  local ok, hex = value_to_hex(rgb)
-  if not ok then
-    return false, 'rgb must be color-code.'
-  end
-  local r = tonumber(hex:sub(1, 2), 16)
-  local g = tonumber(hex:sub(3, 4), 16)
-  local b = tonumber(hex:sub(5, 6), 16)
-
   if attenuation < 0 or attenuation > 100 then
     return false, 'Invalid attenuation value'
   end
 
-  attenuation = (attenuation / 100) * 255
+  local ok, hex = value_to_hex(rgb)
+  if not ok then
+    return false, 'rgb must be color-code.'
+  end
+
+  local r = tonumber(hex:sub(2, 3), 16)
+  local g = tonumber(hex:sub(4, 5), 16)
+  local b = tonumber(hex:sub(6, 7), 16)
+
+  attenuation = attenuation / 100
 
   if vim.go.background == 'light' then
-    r = math.min(255, r + attenuation * (1 - r / 255))
-    g = math.min(255, g + attenuation * (1 - g / 255))
-    b = math.min(255, b + attenuation * (1 - b / 255))
+    r = math.min(255, r + attenuation * (255 - r))
+    g = math.min(255, g + attenuation * (255 - g))
+    b = math.min(255, b + attenuation * (255 - b))
   else
-    r = math.max(0, r - attenuation)
-    g = math.max(0, g - attenuation)
-    b = math.max(0, b - attenuation)
+    r = math.max(0, r - attenuation * r)
+    g = math.max(0, g - attenuation * g)
+    b = math.max(0, b - attenuation * b)
   end
 
   return true, ('#%02X%02X%02X'):format(r, g, b)
